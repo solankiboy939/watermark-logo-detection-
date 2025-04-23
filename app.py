019c16d761f3516fc2f250dc7744f6c8d4ac8391
Manohar Solanki <119101769+solankiboy939@users.noreply.github.com>
2025-04-23 19:33:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
 if uploaded_file:
     # Display image
     image = Image.open(uploaded_file).convert("RGB")  # ensure it's RGB
-    st.image(image, caption="Uploaded Image", use_column_width=True)
+    st.image(image, caption="Uploaded Image", use_container_width=True)
 
     if st.button("Run Detection"):
         st.write("🧠 Detecting watermark logos...")
